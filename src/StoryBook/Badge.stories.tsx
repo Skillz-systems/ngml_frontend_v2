2d89dc894ce1d7a70f566420f33319e5c821bcec
Joynas <Ibinichinasajoy@gmail.com>
2024-01-15 22:42:37 +0100

    added-storybook

---
+++ src/StoryBook/Badge.stories.tsx
@@ -0,0 +1,47 @@
+// import { action } from '@storybook/addon-actions';
+// import Badge from '../Components/Badge';
+
+// export default {
+//   title: 'Components/Badge',
+//   component: Badge,
+// };
+
+// // Define template for your stories
+// const Template = (args) => <Badge {...args} />;
+
+// // Primary Badge
+// export const Primary = Template.bind({});
+// Primary.args = {
+//   type: 'primary',
+//   label: 'Dangote sugar',
+// };
+
+// // Secondary Badge
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   type: 'secondary',
+//   label: 'Dangote sugar',
+// };
+
+// // Transparent Badge
+// export const Transparent = Template.bind({});
+// Transparent.args = {
+//   type: 'transparent',
+//   label: 'Dangote sugar',
+// };
+
+// // Outline Badge
+// export const Outline = Template.bind({});
+// Outline.args = {
+//   type: 'outline',
+//   label: 'Dangote sugar',
+// };
+
+// // Badge with Icon
+// export const WithIcon = Template.bind({});
+// WithIcon.args = {
+//   type: 'primary',
+//   label: 'Dangote sugar',
+//   icon: <YourIconComponent />,
+//   onIconClick: action('Icon Clicked'), // Use Storybook action to log the click event
+// };
