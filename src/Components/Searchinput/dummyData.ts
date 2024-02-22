aa2ef943243364ddfc693d6e6a7109667cd1b75d
Edwin Osayande <navpriman@yahoo.com>
2024-02-22 00:29:12 -0800

    Search Input

--- src/Components/Searchinput/dummyData.ts
+++ src/Components/Searchinput/dummyData.ts
@@ -4,9 +4,9 @@ export interface SearchResult {
 }
 
 export const dummyData: SearchResult[] = [
-    { id: 1, name: 'which kind life be this' },
-    { id: 2, name: 'how person go dey untop sysrtem from morning to night' },
-    { id: 3, name: 'shit eno fit shit' },
-    { id: 4, name: 'baf e no even fit baf' },
-    { id: 5, name: 'food nko na wahala. my people na so e dey be?' },
+    { id: 1, name: 'Apple' },
+    { id: 2, name: 'Banana' },
+    { id: 3, name: 'Orange' },
+    { id: 4, name: 'Grapes' },
+    { id: 5, name: 'Pineapple' },
 ];
