cfc7c44bb9fcfb27d8552ba518606bd957c742e4
Joynas <Ibinichinasajoy@gmail.com>
2024-03-15 18:18:51 +0100

    mofified-tendertable-test

--- src/Components/TableComponent/TenderTable/TenderTable.test.tsx
+++ src/Components/TableComponent/TenderTable/TenderTable.test.tsx
@@ -1,18 +1,8 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import TenderTable from './TenderTable'; // Adjust the import path as necessary
-import { TenderTypeData } from '@/Data'; // Adjust the import path as necessary
+import TenderTable from './TenderTable'; 
 
-// Mock any external dependencies here, if necessary
 
 describe('TenderTable Component', () => {
-  test('renders the data grid with initial data', () => {
-    render(<TenderTable />);
-    
-    // Check if the component properly rendered the rows from TenderTypeData
-    TenderTypeData.forEach((data) => {
-      expect(screen.getByText(data.companyname)).toBeInTheDocument();
-    });
-  });
 
   test('filters rows based on search input', () => {
     render(<TenderTable />);
