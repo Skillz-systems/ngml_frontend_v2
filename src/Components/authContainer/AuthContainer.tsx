578465e49150b8dc4f9a2d9d82a5d91e93d32677
Edwin Osayande <navpriman@yahoo.com>
2024-03-17 12:49:00 -0700

    Password Reset Page

--- src/Components/authContainer/AuthContainer.tsx
+++ src/Components/authContainer/AuthContainer.tsx
@@ -13,38 +13,38 @@ import React from 'react';
  */
 
 interface AuthContainerProps {
-  children: React.ReactNode;
-  backgroundColor?: string;
-  width?: string;
-  height?: string;
+    children: React.ReactNode;
+    backgroundColor?: string;
+    width?: string;
+    height?: string;
 }
 
 const AuthContainer: React.FC<AuthContainerProps> = ({
-  children,
-  backgroundColor = 'rgba(255, 255, 255, 0.5)',
-  width = '400px',
-  height = 'auto',
+    children,
+    backgroundColor = 'rgba(255, 255, 255, 0.5)',
+    width = '400px',
+    height = 'auto',
 }) => {
-  return (
-    <div
-      className="flex justify-center items-center min-h-screen"
-      style={{ backgroundColor }}
-    >
-      <div
-        className="max-w-560 w-full mx-auto p-20 shadow-md rounded-[20px] flex flex-col items-center relative"
-        style={{ width, height }}
-      >
-        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <img
-            src="assets/nnpclogo.png"
-            alt="NGML Logo"
-            className="w-20 h-20 rounded-full"
-          />
+    return (
+        <div
+            className="flex justify-center items-center min-h-screen"
+            style={{ backgroundColor }}
+        >
+            <div
+                className="max-w-560 w-full mx-auto p-20 shadow-md rounded-[20px] flex flex-col items-center relative"
+                style={{ width, height }}
+            >
+                <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    <img
+                        src="assets/nnpclogo.png"
+                        alt="NGML Logo"
+                        className="w-20 h-20 rounded-full"
+                    />
+                </div>
+                {children}
+            </div>
         </div>
-        {children}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AuthContainer;
