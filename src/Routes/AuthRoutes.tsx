d030d7e1fb362dbd92e9274c19e17343e087909e
Edwin Osayande <navpriman@yahoo.com>
2024-03-26 06:34:05 -0700

    Dasboard Customer Route Focus

--- src/Routes/AuthRoutes.tsx
+++ src/Routes/AuthRoutes.tsx
@@ -1,15 +1,28 @@
+import PasswordResetPage from '@/Pages/Passwordreset/PasswordResetPage';
 import ForgotPassword from '@/Pages/Forgotpassword/ForgotPassword';
+import PortalEnrollment from '@/Pages/Portalenrollment/PortalEnrollment';
+import StaffLoginPage from '@/Pages/Stafflogin/StaffLoginPage';
 
 
 export const AuthRoutes = [
-    // {
-    //     title: 'passwordRest',
-    //     path: '/',
-    //     component: PasswordResetPage
-    // },
+    {
+        title: 'passwordRest',
+        path: '/',
+        component: PasswordResetPage
+    },
     {
         title: 'forgotpassword',
         path: '/',
         component: ForgotPassword
+    },
+    {
+        title: 'portalenrollment',
+        path: '/',
+        component: PortalEnrollment
+    },
+    {
+        title: 'staffloginpage',
+        path: '/',
+        component: StaffLoginPage
     },
 ]
