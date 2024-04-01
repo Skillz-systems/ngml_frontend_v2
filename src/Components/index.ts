7cd033a9b697c6790a4f3d64d73f07e7f57c99d1
Joynas <Ibinichinasajoy@gmail.com>
2024-04-01 19:03:21 +0100

    admin-dashboard-modification

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,4 +1,3 @@
 export { default as Badge } from './BadgeComponent/Badge';
 export { default as CustomInput } from './Custominput/CustomInput';
-export { default as ActivityLogCard } from '/Activitylogcard/ActivityLogCard';
 
