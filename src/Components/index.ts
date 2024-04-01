0ed7050c0a054e52c3a98d3050cdfaed2b29d357
Collins Nwoko C <collinsnwoko05@gmail.com>
2024-04-01 09:39:20 +0100

    forgot page fixed

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,6 @@
+export { default as AuthContainer } from './Authcontainer/AuthContainer';
 export { default as Badge } from './BadgeComponent/Badge';
+export { default as Button } from './ButtonComponent/Button';
+export { default as ContentContainer } from './Contentcontainer/ContentContainer';
 export { default as CustomInput } from './Custominput/CustomInput';
 
