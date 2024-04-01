8830fa53724a63c05213b156900217b712e2f285
Collins Nwoko C <collinsnwoko05@gmail.com>
2024-04-01 13:47:00 +0100

    color updated

--- src/Pages/BusinessPage.tsx
+++ src/Pages/BusinessPage.tsx
@@ -1,8 +1,13 @@
 import images from '../assets/index';
-import { Badge } from '../Components/index';
-
+import { Badge, Heading } from '../Components/index';
+import { getColorNames, getColorShades, getShadeNames, useColor } from '../Utils/colors';
 
 const BusinessPage = () => {
+
+  const primaryColor = useColor('nnpc', 500); // returns '#EC0000'
+  const nncpShades = getColorShades('nnpc'); // returns { 100: '#00af50', 200: '#226844', ... }
+  const colorNames = getColorNames(); // returns ['nnpc', 'gray', 'medium', 'dark', 'light', 'red', 'yellow']
+  const mediumShadeNames = getShadeNames('medium'); // returns ['50', '100', '200', '300', '400', '500', '600', '700', '800', '900', '950']
   return (
     <div>
       BusinessPage !!!
@@ -15,6 +20,19 @@ const BusinessPage = () => {
         fontSize='16px'
         radius='8px'
         fontWeight='bold' />
+
+      <div>
+        <Heading as="h1" size="h2" className='italic'>
+          This is a Heading <span className='text-medium-400'>Inside Heading Element</span>
+        </Heading>
+        <Heading as="div" size="h4" className='text-medium-400'>
+          This is a Heading
+        </Heading>
+        <h1 style={{ color: primaryColor }}>Heading</h1>
+        <p>NNPC Shades: {JSON.stringify(nncpShades)}</p>
+        <p>Color Names: {colorNames.join(', ')}</p>
+        <p>Medium Shade Names: {mediumShadeNames.join(', ')}</p>
+      </div>
     </div>
   );
 }
--- src/Utils/colors.ts
+++ src/Utils/colors.ts
@@ -1,15 +1,105 @@
+// FIXME: for usage go to Badge component or BusinessPage
 const colors = {
-  nnpc50: '#f2fbf6',
-  nnpc100: '#e1f7ea',
-  nnpc200: '#c4eed8',
-  nnpc300: '#95e0b8',
-  nnpc400: '#60c892',
-  nnpc500: '#3aad71',
-  nnpc600: '#2b8e5a',
-  nnpc700: '#226844',
-  nnpc800: '#22593d',
-  nnpc900: '#1d4a34',
-  nnpc950: '#0b281a',
+  nnpc: {
+    50:'#D2F69E',
+    100: '#00af50',
+    200: '#226844',
+    300: '#004010',
+    400: '#E9E500',
+    500: '#EC0000',
+  },
+  gray: {
+    50:'#ffffff',
+    100: '#EAEEF2',
+    200: '#49526A',
+    500: '#050505',
+  },
+  medium: {
+    50: '#f2fbf6',
+    100: '#e1f7ea',
+    200: '#c4eed8',
+    300: '#95e0b8',
+    400: '#60c892',
+    500: '#3aad71',
+    600: '#2b8e5a',
+    700: '#226844',
+    800: '#22593d',
+    900: '#1d4a34',
+    950: '#0b281a',
+  },
+  dark: {
+    50: '#eefff0',
+    100: '#d8ffe0',
+    200: '#b4fec2',
+    300: '#79fc92',
+    400: '#38f05d',
+    500: '#0ed938',
+    600: '#05b428',
+    700: '#088d24',
+    800: '#0c6f22',
+    900: '#0c5b1e',
+    950: '#004010',
+  },
+  light: {
+    50: '#eefff5',
+    100: '#d7ffe9',
+    200: '#b2ffd4',
+    300: '#76ffb3',
+    400: '#33f58b',
+    500: '#09de6a',
+    600: '#00af50',
+    700: '#049145',
+    800: '#0a713a',
+    900: '#0a5d32',
+    950: '#00341a',
+  },
+  red: {
+    50: '#fff0f0',
+    100: '#ffdddd',
+    200: '#ffc0c0',
+    300: '#ff9494',
+    400: '#ff5757',
+    500: '#ff2323',
+    600: '#ec0000',
+    700: '#d70000',
+    800: '#b10303',
+    900: '#920a0a',
+    950: '#500000',
+  },
+  yellow: {
+    50: '#fcffe7',
+    100: '#f5ffc1',
+    200: '#f0ff86',
+    300: '#efff41',
+    400: '#f6ff0d',
+    500: '#e9e500',
+    600: '#d1bb00',
+    700: '#a68802',
+    800: '#896a0a',
+    900: '#74560f',
+    950: '#442e04',
+  },
+};
+
+type ColorName = keyof typeof colors;
+type ColorShade = keyof typeof colors.nnpc;
+
+const useColor = (colorName: ColorName, colorShade: ColorShade) => {
+  return colors[colorName][colorShade] as string;
+};
+
+const getColorShades = (colorName: ColorName) => {
+  return colors[colorName];
+};
+
+const getColorNames = () => {
+  return Object.keys(colors) as ColorName[];
+};
+
+const getShadeNames = (colorName: ColorName) => {
+  return Object.keys(colors[colorName]) as unknown as ColorShade[];
 };
 
 export default colors;
+export { getColorNames, getColorShades, getShadeNames, useColor };
+
