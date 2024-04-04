4eff9659d19edbf9c9fa6ee4a61c8c278233bdeb
Edwin Osayande <navpriman@yahoo.com>
2024-04-04 05:14:00 -0700

    Customer Detail

--- src/Routes/AdminRoutes.tsx
+++ src/Routes/AdminRoutes.tsx
@@ -1,5 +1,6 @@
 import AdminDashboard from '@/Pages/AdminDashboardPage/AdminHomePage';
 import BusinessPage from '@/Pages/BusinessPage';
+import CustomerDetailsLayout from '@/Pages/CustomerDetail/CustomerDetailsLayout';
 import ProfilePage from '@/Pages/ProfilePage/Profilepage';
 import AdminInternalLayout from '../Hoc/AdminInternalLayout';
 
