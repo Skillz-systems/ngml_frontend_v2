225bccea53d99cda8865de8d23703516cb2ed06f
Joynas <Ibinichinasajoy@gmail.com>
2024-04-05 14:48:07 +0100

    fixed-adminpage-test-cases

--- src/TestSetup.ts
+++ src/TestSetup.ts
@@ -1,5 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import '@testing-library/jest-dom';
+import '@testing-library/jest-dom';
+import 'resize-observer-polyfill';
 
+class MockResizeObserver {
+  constructor(public callback: ResizeObserverCallback) {}
+
+  observe(target: Element): void {
+  }
+
+  unobserve(target: Element): void {
+  }
+
+  disconnect(): void {
+  }
+}
+
+global.ResizeObserver = MockResizeObserver as any;
 
 
 
