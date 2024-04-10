aa5a478e1ad7ae096edc2440b9d3c56c0f17ef79
Joynas <Ibinichinasajoy@gmail.com>
2024-04-10 23:26:36 +0100

    created-Eoi-page

---
+++ src/Pages/EoiPage/EoiPage.tsx
@@ -0,0 +1,23 @@
+// import SiteVistTableModal from '@/Components/TableComponent/SiteVistTable/SiteVistTableModal'
+
+
+const EoiPage = () => {
+
+    return (
+        <div>
+            Yoo
+            {/* <SiteVistTableModal 
+           dateTime={'09th, Nov, 2023; 09:23:44Am'}
+           status={ 'Default Status'}
+           companyName={ 'Provide Company Name'}
+           companyEmail={ 'Provide an email address'}
+           companyNumber={ 'Provide a number'}
+           availableDates={ ['No Dates Available']}
+           companyAddress={ 'Provide an Address'}
+        //    statusHeading={selectedRow.status} */}
+           
+        </div>
+    )
+}
+
+export default EoiPage
