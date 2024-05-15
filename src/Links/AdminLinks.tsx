790c349de7e93fc7581034b661f78450aaf1aad8
Edwin Osayande <navpriman@yahoo.com>
2024-05-15 03:30:07 -0700

    Assigning proper icons

--- src/Links/AdminLinks.tsx
+++ src/Links/AdminLinks.tsx
@@ -1,6 +1,16 @@
 import Businessicon from '../../src/assets/images/png-icons/Business.png';
+import Communicationicon from '../../src/assets/images/png-icons/Communication.png';
+import Customericon from '../../src/assets/images/png-icons/Customers.png';
+import Bidicon from '../../src/assets/images/png-icons/Filter.png';
 import Homeicon from '../../src/assets/images/png-icons/Home.png';
-
+import Invoiceicon from '../../src/assets/images/png-icons/Invoice.png';
+import Requesticon from '../../src/assets/images/png-icons/Notifications.png';
+import Profilenameicon from '../../src/assets/images/png-icons/Profile.png';
+import Recordicon from '../../src/assets/images/png-icons/Records.png';
+import Reporticon from '../../src/assets/images/png-icons/Reports.png';
+import Settingicon from '../../src/assets/images/png-icons/Settings.png';
+import Suppliericon from '../../src/assets/images/png-icons/Suppliers.png';
+import Sitevisiteicon from '../../src/assets/images/png-icons/Zone.png';
 
 export const AdminLinks = [
   { id: 1, name: 'Home', to: '/admin', icon: Homeicon, type: 'primary' },
@@ -23,27 +33,23 @@ export const AdminLinks = [
     id: 3,
     name: 'Records',
     to: '',
-    icon: Businessicon,
+    icon: Recordicon,
     type: 'secondary',
     subMenu: [
-      { id: 1, name: 'Suppliers', to: '/admin/records/suppliers', icon: Businessicon, type: 'primary' },
-      { id: 2, name: 'Customers', to: '/admin/records/customer', icon: Businessicon, type: 'primary' },
-      { id: 3, name: 'Request', to: '', icon: Businessicon, type: 'primary' },
-      { id: 4, name: 'Site Visits', to: '', icon: Businessicon, type: 'primary' },
-      { id: 5, name: 'Connect Projects', to: '', icon: Businessicon, type: 'primary' },
-      { id: 6, name: 'Bids', to: '', icon: Businessicon, type: 'primary' }
+      { id: 1, name: 'Suppliers', to: '/admin/records/suppliers', icon: Suppliericon, type: 'primary' },
+      { id: 2, name: 'Customers', to: '/admin/records/customer', icon: Customericon, type: 'primary' },
+      { id: 3, name: 'Request', to: '', icon: Requesticon, type: 'primary' },
+      { id: 4, name: 'Site Visits', to: '', icon: Sitevisiteicon, type: 'primary' },
+      { id: 5, name: 'Connect Projects', to: '', icon: Invoiceicon, type: 'primary' },
+      { id: 6, name: 'Bids', to: '', icon: Bidicon, type: 'primary' }
 
 
     ]
   },
-  { id: 4, name: 'Reports', to: '', icon: Homeicon, type: 'primary' },
-  { id: 5, name: 'Communications', to: '', icon: Homeicon, type: 'primary' },
-  { id: 6, name: 'Profile', to: '', icon: Homeicon, type: 'primary' },
-  { id: 7, name: 'Settings', to: '', icon: Homeicon, type: 'primary' },
-
-
-  // { id: 6, name: 'Existing Customer', to: '/admin/customerpagelayout', icon: customers, type: 'primary' },
-  // { id: 7, name: 'Customer Layout', to: '/admin/customerlayout', icon: customers, type: 'primary' },
+  { id: 4, name: 'Reports', to: '', icon: Reporticon, type: 'primary' },
+  { id: 5, name: 'Communications', to: '', icon: Communicationicon, type: 'primary' },
+  { id: 6, name: 'Profile', to: '', icon: Profilenameicon, type: 'primary' },
+  { id: 7, name: 'Settings', to: '', icon: Settingicon, type: 'primary' },
 
 ];
 
