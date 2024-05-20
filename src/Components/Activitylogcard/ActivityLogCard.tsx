d82e57eab597b2f7c04cc515748dc86e98e440a1
Joynas <Ibinichinasajoy@gmail.com>
2024-05-20 14:55:23 +0100

    fixes

--- src/Components/Activitylogcard/ActivityLogCard.tsx
+++ src/Components/Activitylogcard/ActivityLogCard.tsx
@@ -23,15 +23,15 @@ const ActivityLogCard: React.FC<ActivityLogCardProps> = ({ title, text, dateTime
 
   return (
     <div className='w-[100%] mt-[10px]'>
-      <div className={`bg-white w-[100%] h-[100%] shadow-lg  border-1 rounded-[8px] ${button ? 'p-12 mb-4 relative' : 'p-2 mb-4 relative'} `}>
+      <div className={`bg-white w-[100%] h-[100%]  border border-[#E2E4EB] rounded-[8px] ${button ? 'p-12 mb-4 relative' : 'p-2 mb-4 relative'} `} >
         <div className="flex justify-between mb-[3px]" >
           {button ?
             <div className="bg-customGreen text-gray-800 font-[400] mt-[-35px] ml-[-30px] rounded px-2 py-1">{title}</div>
 
             :
             <div className="bg-[#D2F69E] text-[#49526A] text-[12px] p-[3px] rounded-[2px] font-[700]">{title}</div>}
-          {button ? <div className="text-gray-600 text-[13px] mt-[-40px] mr-[-40px]">{formattedDateTime}</div>
-            : <div className="text-[#828DA9§] text-[10px] font-[500]">{formattedDateTime}</div>}
+          {button ? <div className="text-[#828DA9] text-[13px] mt-[-40px] mr-[-40px]">{formattedDateTime}</div>
+            : <div className="text-[#828DA9] text-[10px] font-[500]">{formattedDateTime}</div>}
         </div>
         {button ? <div className="text-gray-800 mt-[-10px] ml-[-30px]">{text}</div>
           : <div className="text-[#49526A] text-[14px] font-[500]">{text}</div>}
