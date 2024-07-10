2fa87d0a62ac682fadeffa63eb0ac3c10c84e787
Nastechy <Ibinichinasajoy@gmail.com>
2024-07-11 00:40:29 +0100

    modifications

--- src/Pages/CustomerLocation/CustomerLocation.tsx
+++ src/Pages/CustomerLocation/CustomerLocation.tsx
@@ -1,4 +1,4 @@
-import { Heading } from '@/Components';
+import { Heading, StatisticCard } from '@/Components';
 import { CompanyAddressData } from '@/Data';
 import colors from '@/Utils/colors';
 import React from 'react';
@@ -7,26 +7,29 @@ import { useNavigate } from 'react-router-dom';
 const CustomerLocation: React.FC = () => {
     const navigate = useNavigate();
 
-    const handleLocationClick = () => {
+    const handleLocationClick = (id: number) => {
         navigate(`/admin/records/customer/id`);
     };
 
     return (
-        <div className='h-fit w-[100%] rounded-[20px] px-6 py-5' >
+        <div className='h-fit w-[100%] rounded-[20px] px-6 py-6' >
             <Heading color='primaryColor' className="font-bold text-gray-600 text-[20px]">COMPANY LOCATIONS</Heading>
-            <div className='w-[100%] flex items-center grid md:grid-cols-2 grid-cols-1 gap-y-4 mt-6'>
-                {CompanyAddressData.map((location) => (
+            <div  className=" grid grid-cols-1 md:grid-cols-2 gap-5 mt-6">
+                {CompanyAddressData.map((card, index) => (
                     <div
-                        className='border md:h-[100px] h-fit md:w-[500px] w-[100%] border-nnpcdarkgreen-500 rounded-[20px] p-[20px] bg-dark-50'
-                        key={location.id}
-                        onClick={() => handleLocationClick()}
-                        style={{ cursor: 'pointer', marginBottom: '10px', background: colors.dark[50] }}
-                    >
-                        <h2 className='text-[20px] font-semibold text-gray-600'>{location.name}</h2>
-                        <p className='text-[14px] font-700'>{location.address}</p>
-                    </div>
+                    key={index}
+                    onClick={() => handleLocationClick(card.id)}
+                    className="cursor-pointer hover:shadow-lg rounded-[20px] transition-shadow duration-600 ease-in-out"
+                  >
+                    <StatisticCard
+                      label={card.name}
+                      value={card.address}
+                      primary={card.primary}
+                    />
+                  </div>
                 ))}
             </div>
+
         </div>
     );
 };
