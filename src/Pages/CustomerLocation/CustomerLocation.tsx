6347e9102a160545a1dc651cbc3436477e68f7ef
Nastechy <Ibinichinasajoy@gmail.com>
2024-07-10 16:54:32 +0100

    modifactions

--- src/Pages/CustomerLocation/CustomerLocation.tsx
+++ src/Pages/CustomerLocation/CustomerLocation.tsx
@@ -12,9 +12,9 @@ const CustomerLocation: React.FC = () => {
     };
 
     return (
-        <div className='h-fit w-[100%] rounded-[20px] px-6 py-6' >
-            <Heading as="h4" size="h4" color='primaryColor' className="font-semibold text-gray-600">COMPANY LOCATIONS</Heading>
-            <div className='w-[100%] flex items-center flex-col gap-5 mt-4'>
+        <div className='h-fit w-[100%] rounded-[20px] px-6 py-5' >
+            <Heading color='primaryColor' className="font-bold text-gray-600 text-[20px]">COMPANY LOCATIONS</Heading>
+            <div className='w-[100%] flex items-center grid md:grid-cols-2 grid-cols-1 gap-y-4 mt-6'>
                 {CompanyAddressData.map((location) => (
                     <div
                         className='border md:h-[100px] h-fit md:w-[500px] w-[100%] border-nnpcdarkgreen-500 rounded-[20px] p-[20px] bg-dark-50'
