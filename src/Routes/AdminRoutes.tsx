e220f07955aece4b85e60b472033f8ba25f58bb4
Edwin Osayande <navpriman@yahoo.com>
2024-07-11 06:34:25 -0700

    supplier url update 2

--- src/Routes/AdminRoutes.tsx
+++ src/Routes/AdminRoutes.tsx
@@ -51,7 +51,7 @@ export const AdminRoutes = [
   },
   {
     title: 'Admin Supplier Registration',
-    path: '/admin/records/supplier/id',
+    path: '/admin/records/supplier/:supplierId/:projectId/:tabId',
     component: AdminInternalLayout(SupplierOverviewLayout),
     exact: true
   },
