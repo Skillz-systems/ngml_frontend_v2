1d4517f2dedec216321dc6655248c3a2f4c76ea7
Nastechy <Ibinichinasajoy@gmail.com>
2024-07-15 12:57:36 +0100

    modifications

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#FAFAFA]">
       <h1 className="text-6xl font-bold text-gray-800 mb-4">404 - Not Found</h1>
       <p className="text-xl text-gray-600">Sorry, the page you are looking for does not exist.</p>
     </div>
