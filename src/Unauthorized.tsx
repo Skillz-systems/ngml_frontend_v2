0468950c364756d0f31062f705239f456b6e8c8c
Nastechy <Ibinichinasajoy@gmail.com>
2024-07-15 13:07:38 +0100

    created-unauthorized-page

--- src/Unauthorized.tsx
+++ src/Unauthorized.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Unauthorized: React.FC = () => {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold text-gray-800 mb-4">401 - Unauthorized</h1>
-      <p className="text-xl text-gray-600">You do not have permission to view this page.</p>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-[#EEEEF0]">
+            <h1 className="text-5xl font-bold text-gray-800 mb-4">401 - Unauthorized</h1>
+            <p className="text-xl text-gray-600">You do not have permission to view this page.</p>
+        </div>
+    );
 };
 
 export default Unauthorized;
