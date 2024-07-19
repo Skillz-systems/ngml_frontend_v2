c9920a7adeccf88bc05d2f45d2b12fd8ea9989d1
Nastechy <Ibinichinasajoy@gmail.com>
2024-07-19 14:16:09 +0100

    created-location-button

--- src/Pages/CustomerLocation/CustomerLocation.tsx
+++ src/Pages/CustomerLocation/CustomerLocation.tsx
@@ -14,13 +14,18 @@ const CustomerLocation: React.FC = () => {
 
   return (
     <div className='mt-6'>
-      <div className='flex gap-4'>
-        <Link to={'/admin/records/customer'}>
-          <div className='flex justify-center items-center border-2 h-[32px] w-[32px] rounded-[50%]'>
-            <ArrowBack color="success" style={{ fontSize: 'medium' }} />
-          </div>
-        </Link>
-        <Heading color='primaryColor' className="font-bold text-gray-600 text-[23px]">COMPANY LOCATIONS</Heading>
+      <div className='flex justify-between'>
+        <div className='flex gap-4'>
+          <Link to={'/admin/records/customer'}>
+            <div className='flex justify-center items-center border-2 h-[32px] w-[32px] rounded-[50%]'>
+              <ArrowBack color="success" style={{ fontSize: 'medium' }} />
+            </div>
+          </Link>
+          <Heading color='primaryColor' className="font-bold text-gray-600 text-[23px]">COMPANY LOCATIONS</Heading>
+        </div>
+        <div>
+          <span className="text-sm md:text-base ml-2 cursor-pointer">Upload Agreement</span>
+        </div>
       </div>
       <div className='h-fit w-[100%] rounded-[20px] px-6 ' >
         <div className=" grid grid-cols-1 md:grid-cols-3 gap-5 mt-6">
