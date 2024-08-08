aac70ea6270363c5524d4a679a8721c76ad94db6
Nastechy <Ibinichinasajoy@gmail.com>
2024-08-08 17:49:48 +0100

    resolved-failing-test

--- src/Pages/BidPage/BidPage.test.tsx
+++ src/Pages/BidPage/BidPage.test.tsx
@@ -1,11 +1,16 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom'; 
 import BidPage from './BidPage';
 
 describe('BidPage Component', () => {
 
     it('should render StatisticRectangleCard components', () => {
-        render(<BidPage />);
+        render(
+            <BrowserRouter>
+                <BidPage />
+            </BrowserRouter>
+        );
         const openTendersCard = screen.getByText('Open Tenders');
         const submittedBidsCard = screen.getByText('Submitted Bids');
         expect(openTendersCard).toBeInTheDocument();
