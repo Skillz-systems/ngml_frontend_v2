924dc3d5c737eda73159e58107f10945ec05df18
Colin-nwk <collinsnwoko05@gmail.com>
2024-09-11 10:33:46 +0100

    wip

--- src/Redux/rootReducer.ts
+++ src/Redux/rootReducer.ts
@@ -3,13 +3,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { api } from './api';
 import authSlice from './Features/Auth/authSlice';
-import processFlowSlice from './Features/ProcessFlow/processFlowSlice';
 import userReducer from './userSlice';
 
 const rootReducer = combineReducers({
     user: userReducer,
     auth: authSlice,
-    processFlow: processFlowSlice,
     [api.reducerPath]: api.reducer,
 });
 
