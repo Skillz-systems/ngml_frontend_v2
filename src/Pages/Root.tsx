9d334cfe8bb249cca18ffc2921d1387849d68aaa
Edwin Osayande <navpriman@yahoo.com>
2024-10-04 07:47:44 -0700

    user settings page using tab 1

--- src/Pages/Root.tsx
+++ src/Pages/Root.tsx
@@ -5,30 +5,29 @@ import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { selectCurrentUser, selectToken } from '../Redux/Features/Auth/authSlice';
 
 const Root = () => {
-    // const location = useLocation();
-    // const [isAuthChecked, setIsAuthChecked] = useState(false);
-    // const user = useSelector(selectCurrentUser);
-    // const token = useSelector(selectToken);
+    const location = useLocation();
+    const [isAuthChecked, setIsAuthChecked] = useState(false);
+    const user = useSelector(selectCurrentUser);
+    const token = useSelector(selectToken);
 
-    // useLayoutEffect(() => {
-    //     // This will run synchronously before the browser paints
-    //     setIsAuthChecked(true);
-    // }, []);
+    useLayoutEffect(() => {
+        // This will run synchronously before the browser paints
+        setIsAuthChecked(true);
+    }, []);
 
-    // if (!isAuthChecked) {
-    //     return null; // Return nothing while checking auth
-    // }
+    if (!isAuthChecked) {
+        return null; // Return nothing while checking auth
+    }
 
-    // const isAuthenticated = Boolean(token && user);
+    const isAuthenticated = Boolean(token && user);
 
     return (
         <MainLayout>
-            {/* {isAuthenticated ? (
-               
+            {isAuthenticated ? (
+                <Outlet />
             ) : (
                 <Navigate to="/" state={{ from: location }} replace />
-            )} */}
-             <Outlet />
+            )}
         </MainLayout>
     );
 };
