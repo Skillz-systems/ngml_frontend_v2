c3f3c88c0607652f5d1aef0e0b7c199246aa1eec
Nastechy <Ibinichinasajoy@gmail.com>
2024-10-14 10:26:36 +0100

    modifications

--- src/Components/TableComponent/DailyVolumnTable/DailyVolumnTable.test.tsx
+++ src/Components/TableComponent/DailyVolumnTable/DailyVolumnTable.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { describe, it } from 'vitest';
 import DailyVolumnTable from './DailyVolumnTable';
 import { Provider } from 'react-redux';
