e5353fa663c4459d2f663c25d62ebc1c4c0ffeb1
Nastechy <Ibinichinasajoy@gmail.com>
2024-10-25 00:40:39 +0100

    modifications

--- src/Redux/api.ts
+++ src/Redux/api.ts
@@ -14,14 +14,13 @@ import { toast } from 'react-toastify';
 const baseQuery = fetchBaseQuery({
   baseUrl: 'https://api.ngml.skillzserver.com',
   prepareHeaders: (headers, { getState }) => {
-    const token = (getState() as any).auth.access_token;
-    
-    console.log(token)
-    // if (token) {
-    //   headers.set('authorization', `Bearer ${token}`);
-    // }
+    const token = (getState() as any).auth.access_token;    
+    // console.log(token)
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`);
+    }
 
-    headers.set('authorization', 'Bearer 19|ENEsiIwt9K9AmPM6xLtvfdZ2OLIHWO19RQmvPyAs3089de3b');
+    // headers.set('authorization', 'Bearer 19|ENEsiIwt9K9AmPM6xLtvfdZ2OLIHWO19RQmvPyAs3089de3b');
     return headers;
   },
 
