eede2edff96b919d7ea9f8208b6c92faf221e988
Colin-nwk <collinsnwoko05@gmail.com>
2024-10-25 20:12:10 +0100

    started

--- src/Pages/FormBuilderCroup/FormBuilderPage.tsx
+++ src/Pages/FormBuilderCroup/FormBuilderPage.tsx
@@ -365,23 +365,6 @@ const EditableFormElement = ({
                             </div>
                         </div>
                     )}
-
-                    {/* {(editedElement.type === 'text' || editedElement.type === 'textarea') && (
-                        <div>
-                            <label className="block text-sm font-medium mb-1">
-                                Placeholder
-                            </label>
-                            <input
-                                type="text"
-                                value={editedElement.placeholder || ''}
-                                onChange={(e) =>
-                                    handleInputChange('placeholder', e.target.value)
-                                }
-                                className="flex h-10 w-full rounded-md border-[1.5px] border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-light-green disabled:cursor-not-allowed disabled:opacity-50"
-                            />
-                        </div>
-                    )} */}
-
                     {(editedElement.type === 'dynamic') && (
                         <div>
                             <label className="block text-sm font-medium mb-1">
@@ -437,25 +420,9 @@ const FormBuilder = () => {
     const [formElements, setFormElements] = useState<FormElement[]>([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const { data: backendForms } = useGetFormsQuery();
-    // const [selectedForm, setSelectedForm] = useState(null);
     const [selectedForm, setSelectedForm] = useState<FormData | null>(null);
     const [createForm, { isLoading }] = useCreateFormMutation();
     const [updateForm] = useUpdateFormMutation();
-
-    // const handleFormSelection = (form: any) => {
-    //     setSelectedForm(form);
-    //     setForm({
-    //         name: form.name,
-    //         description: form.description,
-    //         process_flow_id: form.process_flow_id,
-    //         process_flow_step_id: form.process_flow_step_id,
-    //         tag_id: form.tag_id,
-    //         json_form: form.json_form,
-    //         json_data: form.json_data,
-    //     });
-    //     setFormElements(JSON.parse(form.json_form));
-    //     setIsModalOpen(false);
-    // };
     const handleFormSelection = (form: FormData) => {
         setSelectedForm(form);
         setForm({
@@ -547,40 +514,6 @@ const FormBuilder = () => {
         }
         return selectedForm ? 'Update Form' : 'Create Form';
     };
-
-    //TODO old
-    // const handleSaveForm = async () => {
-    //     const newForm: FormInterface = {
-    //         name: form.name,
-    //         description: form.description,
-    //         process_flow_id: form.process_flow_id,
-    //         process_flow_step_id: form.process_flow_step_id,
-    //         tag_id: form.tag_id,
-    //         json_form: JSON.stringify(formElements),
-    //         // json_form: formElements,
-    //         json_data: [],
-    //     };
-    //     // console.log(formElements);
-
-    //     if (form?.name?.trim() === '' || form?.description?.trim() === '' || form?.process_flow_id === '' || form?.tag_id?.trim() === '' || form?.process_flow_step_id === '') {
-    //         toast.error('All field are required');
-
-    //         return;
-    //     }
-
-
-    //     try {
-    //         await createForm(newForm).unwrap();
-    //         // console.log(newForm)
-    //         toast.success('form created successfully!');
-    //     } catch (error) {
-    //         console.error('Error submitting the process flow:', error);
-    //         toast.error('Error creating form');
-    //     }
-
-    //     // alert('Form saved successfully!');
-    //     handleCreateNewForm();
-    // };
     const handleSaveForm = async () => {
         const formData = {
             name: form.name,
@@ -599,13 +532,10 @@ const FormBuilder = () => {
 
         try {
             if (selectedForm) {
-                // Update existing form
-                // await updateForm({selectedForm.id, ...formData}).unwrap();
+
                 await updateForm({ id: selectedForm.id, ...formData }).unwrap();
-                // await updateForm(formData).unwrap();
                 toast.success('Form updated successfully!');
             } else {
-                // Create new form
                 await createForm(formData).unwrap();
                 toast.success('Form created successfully!');
             }
@@ -623,11 +553,6 @@ const FormBuilder = () => {
     return (
         <div className="flex flex-col h-full bg-transparent w-full rounded-xl">
             <div className="flex items-center justify-between p-1 mb-2 ">
-                {/* <Link to={'/admin/settings'}>
-                    <div className="flex justify-center items-center border-2 h-[32px] w-[32px] rounded-[50%]">
-                        <ArrowBack color="success" style={{ fontSize: 'medium' }} />
-                    </div>
-                </Link> */}
                 <Link to={'/admin/settings'}>
                     <div className="flex justify-center items-center border-2 h-[32px] w-[32px] rounded-[50%]">
                         <ArrowBack color="success" style={{ fontSize: 'medium' }} />
@@ -636,10 +561,6 @@ const FormBuilder = () => {
                 <h1 className="text-lg font-bold">
                     {selectedForm ? `Editing Form: ${selectedForm.name}` : 'Form Builder'}
                 </h1>
-
-                {/* <div>
-                    <h1 className="text-lg font-bold">Form Builder</h1>
-                </div> */}
                 <div className="flex space-x-2">
 
 
@@ -668,15 +589,6 @@ const FormBuilder = () => {
                             <FaTimes className="mr-2" /> Cancel Edit
                         </button>
                     )}
-                    {/* <button
-                        onClick={handleSaveForm}
-                        disabled={isLoading}
-                        className="w-fit px-2 py-1.5 duration-200 ease-in transition-all bg-green-900 text-white rounded hover:bg-green-800 flex items-center text-sm disabled:cursor-not-allowed disabled:opacity-70"
-                    >
-                        <span className="sr-only">Create form</span>
-
-                        <FaSave className="mr-2" /> {isLoading ? 'Creating...' : 'Create Form'}
-                    </button> */}
                     <button
                         onClick={handleSaveForm}
                         disabled={isLoading}
@@ -735,6 +647,32 @@ const FormBuilder = () => {
                         />
 
                         <div>
+                            <label htmlFor="tag_id" className="sr-only">
+                                Tag
+                            </label>
+                            <select
+                                id="tag_id"
+                                name="tag_id"
+                                value={form.tag_id}
+                                onChange={handleFormChange('tag_id')}
+                                className="flex h-10 w-full rounded-md border-[1.5px] border-input bg-white px-3 py-2 text-sm 
+          ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium 
+          placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-light-green 
+          disabled:cursor-not-allowed disabled:opacity-50 placeholder-shown:text-gray-400"
+                            >
+                                <option className="text-gray-500">Select a tag</option>
+                                {Array.isArray(tags?.data) &&
+                                    tags.data.map(
+                                        (tag: { id: Key | null | undefined; name: string }) => (
+                                            <option key={tag.id} value={Number(tag.id)}>
+                                                {tag.name.replace(/_/g, ' ')}
+                                            </option>
+                                        )
+                                    )}
+                            </select>
+                        </div>
+
+                        <div>
                             <label htmlFor="process_flow_id" className="sr-only">
                                 ProcessFlow
                             </label>
@@ -787,31 +725,7 @@ const FormBuilder = () => {
                                 )}
                             </select>
                         </div>
-                        <div>
-                            <label htmlFor="tag_id" className="sr-only">
-                                Tag
-                            </label>
-                            <select
-                                id="tag_id"
-                                name="tag_id"
-                                value={form.tag_id}
-                                onChange={handleFormChange('tag_id')}
-                                className="flex h-10 w-full rounded-md border-[1.5px] border-input bg-white px-3 py-2 text-sm 
-          ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium 
-          placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-light-green 
-          disabled:cursor-not-allowed disabled:opacity-50 placeholder-shown:text-gray-400"
-                            >
-                                <option className="text-gray-500">Select a tag</option>
-                                {Array.isArray(tags?.data) &&
-                                    tags.data.map(
-                                        (tag: { id: Key | null | undefined; name: string }) => (
-                                            <option key={tag.id} value={Number(tag.id)}>
-                                                {tag.name.replace(/_/g, ' ')}
-                                            </option>
-                                        )
-                                    )}
-                            </select>
-                        </div>
+
                     </div>
 
                     <h2 className="text-base font-bold mb-4">Form Elements</h2>
