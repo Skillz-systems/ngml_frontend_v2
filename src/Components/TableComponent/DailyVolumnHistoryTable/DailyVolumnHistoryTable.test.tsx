d1ec478c0bd52300e753ff91e356c334f8b61b60
Nastechy <Ibinichinasajoy@gmail.com>
2024-11-08 11:18:24 +0100

    resolved-failed-test

--- src/Components/TableComponent/DailyVolumnHistoryTable/DailyVolumnHistoryTable.test.tsx
+++ src/Components/TableComponent/DailyVolumnHistoryTable/DailyVolumnHistoryTable.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import { describe, expect, it, vi } from 'vitest';
+import { render } from '@testing-library/react';
+import { describe, it, vi } from 'vitest';
 import { Provider } from 'react-redux';
 import DailyVolumnHistoryTable from './DailyVolumnHistoryTable';
 import { store } from '@/Redux/store';
